b34eac1df9bb9d227864bee4922c030160abada8
Jonathan Sax <jsax8310@outlook.com>
2017-06-15 12:55:46 -0600

    stop light finished

--- index.js
+++ index.js
@@ -1,5 +1,54 @@
 (function() {
   'use strict';
+  var stopButton = document.getElementById('stopButton')
+  var stopLight = document.getElementById('stopLight')
+  var slowButton = document.getElementById('slowButton')
+  var slowLight = document.getElementById('slowLight')
+  var goButton = document.getElementById('goButton')
+  var goLight = document.getElementById('goLight')
 
-  // YOUR CODE HERE
+  stopButton.addEventListener('click', function() {
+    if (stopLight.style.backgroundColor === 'black'){
+      stopLight.style.backgroundColor = 'red';
+    }
+    else {
+      stopLight.style.backgroundColor = 'black';
+    }
+  });
+
+  slowButton.addEventListener('click', function() {
+    if (slowLight.style.backgroundColor === 'black') {
+      slowLight.style.backgroundColor = 'yellow'
+    }
+    else {
+      slowLight.style.backgroundColor = 'black'
+    }
+  });
+
+  goButton.addEventListener('click', function() {
+    if (goLight.style.backgroundColor === 'black') {
+      goLight.style.backgroundColor = 'green';
+    }
+    else {
+      goLight.style.backgroundColor = 'black';
+    }
+  });
+  stopButton.addEventListener('mouseover', function(){
+    console.log('Entered Stop Button');
+  })
+  stopButton.addEventListener('mouseleave', function(){
+    console.log('Left Stop Button');
+  })
+  slowButton.addEventListener('mouseover', function(){
+    console.log('Entered Slow Button');
+  })
+  slowButton.addEventListener('mouseleave', function(){
+    console.log('Left Slow Button');
+  })
+  goButton.addEventListener('mouseover', function(){
+    console.log('Entered Go Button');
+  })
+  goButton.addEventListener('mouseleave', function(){
+    console.log('Left Go Button');
+  })
 })();
